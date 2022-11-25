28e5b67b4529eadd3fabf4b213a004f09a1f3c90
IKHARO Paul <paulevansikharo@gmail.com>
2022-11-26 00:06:14 +0100

    delete added

--- main.js
+++ main.js
@@ -27,6 +27,15 @@ let phones = [
       "number": "39-23-6423122"
     }
 ]
+
+let phone;
+
+let pickPhone = (req, items) => {
+    const {id} = req.params
+    return phone = items.filter(p => p.id === +id)
+}
+
+
 // 3.1:
 // Implement a Node application that returns a hardcoded list of phonebook 
 // entries from the address http://localhost:3001/api/persons.
@@ -41,9 +50,10 @@ app.get(`/api/persons`, (req, res) => {
 //
 //${todat's date and time zone}"
 app.get(`/info/`, (req, res) => {
+
     res.setHeader("Content-Type", "text/html")
 
-    res.write("<p>Phonebook has info for 2 people </p>");
+    res.write(`<p>Phonebook has info for ${phones.length} people </p>`);
     res.write(`<p>${new Date()}<p>`);
     res.end()
 })
@@ -54,16 +64,29 @@ app.get(`/info/`, (req, res) => {
 // 
 // If an entry for the given id is not found, the server has to respond with the appropriate status code.
 app.get(`/api/persons/:id`, (req,res) => {
-    let {id} = req.params;
-    let phone = phones.find(phone => phone.id === +id)
+
+    pickPhone(req, phones, phone)
     if (phone) {
         console.log(phone)
         res.json(phone)
     }
-    res.statusMessage = "The phone id is not valid"
-    res.status(400).send(`${res.statusMessage}`)
+    // res.statusMessage = "The phone id is not valid"
+    res.status(404).end()
+})
 
+// 3.4:
+// Implement functionality that makes it possible to delete a single phonebook entry by making 
+// an HTTP DELETE request to the unique URL of that phonebook entry.
+app. delete(`/api/persons/:id`, (req, res) => {
+    pickPhone(req, phones, phone)
+    console.log(`${phones.length} persons are left`)
+
+    res.statusMessage = `phone ${phones.length} has been deleted`
+    res.status(204).end()
+    console.log(`${phones.length} has been deleted`)
 
 })
+
+
 
 app.listen(PORT)

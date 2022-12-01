6e89faf6a59c79797798bae88fee4949ed845456
IKHARO Paul <paulevansikharo@gmail.com>
2022-12-01 14:44:09 +0100

    added client side input

--- main.js
+++ main.js
@@ -1,56 +1,45 @@
 const express = require('express')
+const bodyParser =  require('body-parser')
+const MongoClient = require('mongodb').MongoClient
 const app = express()
 
+  
+app.use(bodyParser.urlencoded({extended: true}))
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
 
-
 let PORT = (process.env.PORT || 3000);
+
+const mongoUrl = 'mongodb+srv://pevans:08050469567@cluster0.qurzf2k.mongodb.net/?retryWrites=true&w=majority'
+MongoClient.connect(mongoUrl)
+  .then(client => console.log('connected to mongobd'))
+  .catch(err => console.error(err))
+
+
+
 // PUT ZIP codes in each of these customers details
-let phones = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
+let phones = []
 
-let newId = () => phones.map(p => p.id).reduce((a, b) => Math.max(a, b), -Infinity);
+//ID generator
+let newId = () => (phones.length > 0) ? phones.map(p => p.id).reduce((a, b) => Math.max(a, b), -Infinity): 0;
 
-
+// Handle form in html file 
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html')
+})
 
 // Implement a Node application that returns a hardcoded list of phonebook 
 // entries from the address http://localhost:3001/api/persons.
-
 app.get(`/api/persons`, (req, res) => {
   res.json(phones)
 })
 
-
 // Implement a page at the address http://localhost:3001/info that looks roughly like this
 // "Phonebook has info for 2 people
-//
-//${todat's date and time zone}"
 app.get(`/info/`, (req, res) => {
   res.send(`<p>Phone book has entry for ${phones.length} people</p>`)
 })
-
 
 // Implement the functionality for displaying the information for a single phonebook entry. 
 // The url for getting the data for a person with the id 5 should be http://localhost:3001/api/persons/5
@@ -67,7 +56,6 @@ app.get(`/api/persons/:id`, (req,res) => {
   }
 })
 
-
 // Implement functionality that makes it possible to delete a single phonebook entry by making 
 // an HTTP DELETE request to the unique URL of that phonebook entry.
 app.delete(`/api/persons/:id`, (req, res) => {
@@ -76,8 +64,6 @@ app.delete(`/api/persons/:id`, (req, res) => {
   console.log(remainingPersons)
   res.status(204).end()
 })
-
-
 
 // Expand the backend so that new phonebook entries can be added by making 
 //HTTP POST requests to the address http://localhost:3001/api/persons.
@@ -88,24 +74,25 @@ app.delete(`/api/persons/:id`, (req, res) => {
 // b. The name already exists in the phonebook
 // Respond to requests like these with the appropriate status code, and also send back 
 // information that explains the reason for the error, :
-
 app.post(`/api/persons`, (req, res) => {
   res.setHeader("Content-Type", "application/json")
   let customer = req.body
 
   customer = {
     id: (newId() + 1),
-    name: "Jemall", 
-    number: "040-123e46"
+    name: customer.name, 
+    number: customer.number,
+    date: new Date()
   }
 
   let existingName = phones.map(p => p.name).find(n => customer.name === n)
-
+  console.log(req.body)
   if (customer.name === null || customer.number === null ||  existingName ) {
     console.log(phones)
-    return res.status(305).send({error: "name must be unique"})
+    return res.status(305).json({error: "name must be unique"})
   }else {
     let totalCustomers = phones.push(customer)
+    console.log(totalCustomers)
     console.log(totalCustomers)
     return res.json(phones)
   }
